291acbeae79cbebb2c1932942b56e41dc84df793
nunoR <nunoribeiroft@outlook.com>
2023-08-21 17:13:10 +0100

    Done

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -3,16 +3,31 @@ const Projects = () => {
     return ( 
         <div className='projectsComponent'>
          <h1>Projects</h1>
-        
+        <div className='linkdironImg'>
          <a href="https://linkdiron.netlify.app/login"  target="_blank" rel="noreferrer"
          style={{ width: '100px'}}>
          <img src='firstImageHome.png' alt='project1' className='linkdironImg'/>
             <h4>LinkdIron</h4>
             <p>Full Stack App</p>
             </a>
-            
+            </div>
+            <div className="fitnessImg">
+            <a href="https://fitnessapp.adaptable.app/"  target="_blank" rel="noreferrer"
+         style={{ width: '100px'}}>
+         <img src='backgroundIMG.jpg' alt='project1' className='fitnessAppImg'/>
+            <h4>FitnessApp</h4>
+            <p>Back-end App</p>
+            </a>
+</div>
+<div className="deadpoolImg">
+            <a href="https://nunomatosribeiro.github.io/deadpool-vs-cows-proj/"  target="_blank" rel="noreferrer"
+         style={{ width: '100px'}}>
+         <img src='deadpoolImg.jpg' alt='project1' className='deadpoolImg'/>
+            <h4>Deadpool Game</h4>
+            <p>Front-end game</p>
+            </a>
 
-           
+           </div>
             
         </div>
      );
