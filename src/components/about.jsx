62231de79abf14800e53f1c012aaec5106a130dc
nunoR <nunoribeiroft@outlook.com>
2023-09-14 12:31:52 +0100

    small text changes

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -8,12 +8,12 @@ const About = () => {
         
         
         <h3>Why Web Dev?</h3>
-        <p>I decided to change my career for an area that will certainly lead the future. I believe changing is a part of life and it fosters personal growth, and it should happen when we feel it is time. </p>
+        <p>I decided to change my career for an area that will certainly lead the future. I believe changing is a part of life, it fosters personal growth, and it should happen when we feel it is time. </p>
             
             <h3>What can I bring to the table?</h3>
             <p>
             Some of my projects included technologies such as HTML, CSS, JavaScript, React, Node.js, MongoDB and more.
-            I believe to be ready to land a job in front-end and help your company grow and stand out from the market.
+            I believe I am ready to land a job in front-end and help your company grow and stand out from the market.
            </p>
            <p>My soft skills include</p>
            <p>Easy adaptability to new work environments</p>
