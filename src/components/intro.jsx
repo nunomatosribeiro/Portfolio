4033dab1cafb88c369cd79b1d8b9c6f7406bd862
nunoR <nunoribeiroft@outlook.com>
2023-09-14 12:11:22 +0100

    Improved styling and changed images

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -1,11 +1,19 @@
 const Intro = () => {
     return ( 
-        <div className="introSection">
-        <h1 >Welcome aboard!</h1>
-        
-        <img className='imgIntro' src='./unsplashimg.jpg' alt='introImg' />
-        
+        <section className="introSection" >
+        <div className="titleDiv">
+
+        <h1>Welcome aboard!</h1>
+        <p>Hi! My name is Nuno Ribeiro and I am a junior front-end developer.  </p>
+        <a href="https://drive.google.com/file/d/1jw3HVB-zZQGD9R7BSEhQwwruF4oAdv6Q/view?usp=sharing" target="_blank" rel= 'noreferrer'>   
+<button className='cv'><span>Resume</span></button>
+</a>  
         </div>
+    <div className="imgDiv">
+        <img className='imgIntro' src='./newyork3.jpeg'  />
+        </div>
+        
+        </section>
      );
 }
  
